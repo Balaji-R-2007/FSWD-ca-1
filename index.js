fd2d0074bf90fa13a1e0edee38bb1556bfef8252
balaji-r-2007 <balaji648balaji@gmail.com>
2025-02-18 10:27:54 +0530

    done

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const PORT=2000;
 app.get('/',(req,res)=>{
     res.send('hi');
 });
+
+app.post('/signup',(req,res)=>{
+    if(!req.username){
+        res.send('username cannot be empty')
+        
+    }
+    if(!req.email){
+        res.send('Email cannot be empty')
+        
+    }
+    if(!req.password || password.length<8 && password.length>16){
+        res.send('password length must be greaterthan 8 and lessthan or equal to 16')
+    }
+});
+
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}`);
 })
